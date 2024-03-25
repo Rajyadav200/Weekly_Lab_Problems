b6cb062f4688575a8b9e6dd163eae6bf122d106a
Raj Yadav <yraj07471@gmail.com>
2024-03-25 16:50:50 +0530

    Problem have been solved

---
+++ week_5_lab/problem_A.py
@@ -0,0 +1,14 @@
+# this calculate the fanctorial of a number 
+
+# User Input 
+fact_num = int(input("Enter a number:"))
+
+for i in range(fact_num+1):
+    if (i == 0):
+        break 
+        fact_n = 1*i
+        fact_num -= 1
+
+     
+
+print(fact_n)
